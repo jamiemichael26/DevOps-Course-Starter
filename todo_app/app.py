4d8a72e1636989b68aaab886d238d6a6cb1767b8
Jamie Michael <jamie.michael@capita.com>
2020-09-04 09:14:08 +0100

    Module-1-Working

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import todo_app.data.session_items as session
 
+from todo_app.flask_config import Config
+
 app = Flask(__name__)
-app.config.from_object('flask_config.Config')
+app.config.from_object(Config)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
