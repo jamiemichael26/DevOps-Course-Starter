b665b0b251123317c06f8ab4837b343d9e14cf8d
Jamie Michael <jamie.michael@capita.com>
2020-09-18 20:25:38 +0100

    Updated the endpoints and added the API

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,38 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
-import todo_app.data.session_items as session
+from flask import Flask, render_template, redirect, url_for, request
 
 from todo_app.flask_config import Config
+from todo_app.data import session_items as session
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        items = session.get_items()
-        return render_template('index.html', myitems=items)
+    items = session.get_items()
+    return render_template('index.html', items = items)
 
-@app.route('/new-item', methods=['POST']) 
-def new_item():
-    if request.form.get('title'):
-        title = request.form.get('title')
-        session.add_item(title)
-    return redirect('/')
+@app.route('/items/new', methods=['POST'])
+def add_item():
+    title = request.form['title']
+    session.add_item(title)
+    return redirect(url_for('index'))
 
-@app.route('/delete-item', methods=['POST']) 
-def delete_item():
-    if request.form.get('del_id'):
-        del_id = int(request.form.get('del_id')) 
-        session.delete_item(del_id -1)
-    return redirect('/')
+@app.route('/items/<id>/complete')
+def complete_item(id):
+    session.complete_item(id)
+    return redirect(url_for('index'))
 
-@app.route('/update-item', methods=['POST']) 
-def update_item():
-    if request.form.get('com_id'):
-        com_item = session.get_item(request.form.get('com_id'))
-        com_item['status'] = 'Complete'
-        session.save_item(com_item)
-    return redirect('/')
+@app.route('/items/<id>/doing')
+def doing_item(id):
+    session.doing_item(id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
